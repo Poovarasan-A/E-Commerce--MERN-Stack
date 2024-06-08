fae0883f50c5843240273b065d1b5b4dbe8ab81d
dark1030 <poovarasanarumugam1030@gmail.com>
2024-06-08 11:29:03 +0530

    Image size fixed mobile

--- frontend/src/components/products/Products.js
+++ frontend/src/components/products/Products.js
@@ -1,27 +1,11 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 // import { FaStar } from "react-icons/fa6";
 
 const Products = ({ product }) => {
-  const [productImage, setProductImage] = useState("");
-  const baseURL = process.env.BACKEND_URL;
-  useEffect(() => {
-    if (product) {
-      if (product.files && product.files[0] && product.files[0].fileName) {
-        setProductImage(
-          `http://localhost:8001/images/${product.files[0].fileName}`
-          // `${baseURL}/images/${product.files[0].fileName}`
-        );
-      } else {
-        setProductImage("");
-      }
-    }
-  }, [product, baseURL]);
-
   return (
     <div>
       <section className="  box-border overflow-hidden whitespace-nowrap w-[11rem] md:w-[12rem] lg:w-[14rem] my-1 lg:my-3 flex flex-col items-center gap-1 lg:gap-3">
-        <div className="lg:w-[14rem] bg-white lg:h-[16rem] flex items-center justify-center">
+        <div className="lg:w-[14rem] bg-white h-[15rem] lg:h-[16rem] flex items-center justify-center">
           <img
             width={180}
             height={180}
